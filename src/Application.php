c5b66161c8e6f286e218a491205a1bd48f06b499
WaveHack <email@wavehack.net>
2017-03-05 19:59:16 +0100

    Version

--- src/Application.php
+++ src/Application.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Application as SymfonyApplication;
 class Application extends SymfonyApplication
 {
     const NAME = 'Meowstic';
-    const VERSION = '0.1.7';
+    const VERSION = '0.1.8';
 
     public function __construct()
     {
